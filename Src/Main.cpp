0440d7e082f15e5a7a53402cb249f74f8171b01a
tn_mai <rabitfiver@gmail.com>
2018-06-19 03:09:22 +0900

    スプライトを表示.

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,11 +1,125 @@
 /**
 * @file Main.cpp
 */
+#include "GLFWEW.h"
+#include "Texture.h"
+#include "Sprite.h"
+
+const char title[] = "OpenGL2D 2018"; // ウィンドウタイトル.
+const int windowWidth = 800; // ウィンドウの幅.
+const int windowHeight = 600; // ウィンドウの高さ.
+
+SpriteRenderer renderer;
+Sprite sprBackground; // 背景用スプライト.
+Sprite sprPlayer;     // 自機用スプライト.
+glm::vec3 playerVelocity; // 自機の移動速度.
+
+void processInput(GLFWEW::WindowRef);
+void update(GLFWEW::WindowRef);
+void render(GLFWEW::WindowRef);
 
 /**
 * プログラムのエントリーポイント.
 */
 int main()
 {
+  // アプリケーションの初期化.
+  GLFWEW::WindowRef window = GLFWEW::Window::Instance();
+  if (!window.Initialize(windowWidth, windowHeight, title)) {
+    return 1;
+  }
+  if (!Texture::Initialize()) {
+    return 1;
+  }
+  if (!renderer.Initialize(1024)) {
+    return 1;
+  }
+
+  // 使用する画像を用意.
+  sprBackground = Sprite("Res/UnknownPlanet.png");
+  sprPlayer = Sprite("Res/Objects.png", glm::vec3(0, 0, 0), Rect(0, 0, 64, 32));
+
+  // ゲームループ.
+  while (!window.ShouldClose()) {
+    processInput(window);
+    update(window);
+    render(window);
+  }
+
+  Texture::Finalize();
   return 0;
 }
+
+/**
+* プレイヤーの入力を処理する.
+*
+* @param window ゲームを管理するウィンドウ.
+*/
+void processInput(GLFWEW::WindowRef window)
+{
+  window.Update();
+
+  // 自機の速度を設定する.
+  const GamePad gamepad = window.GetGamePad();
+  if (gamepad.buttons & GamePad::DPAD_UP) {
+    playerVelocity.y = 1;
+  } else if (gamepad.buttons & GamePad::DPAD_DOWN) {
+    playerVelocity.y = -1;
+  } else {
+    playerVelocity.y = 0;
+  }
+  if (gamepad.buttons & GamePad::DPAD_RIGHT) {
+    playerVelocity.x = 1;
+  } else if (gamepad.buttons & GamePad::DPAD_LEFT) {
+    playerVelocity.x = -1;
+  } else {
+    playerVelocity.x = 0;
+  }
+  if (playerVelocity.x || playerVelocity.y) {
+    playerVelocity = glm::normalize(playerVelocity) * 400.0f;
+  }
+}
+
+/**
+* ゲームの状態を更新する.
+*
+* @param window ゲームを管理するウィンドウ.
+*/
+void update(GLFWEW::WindowRef window)
+{
+  const float deltaTime = window.DeltaTime();
+
+  // 自機の移動.
+  if (playerVelocity.x || playerVelocity.y) {
+    glm::vec3 newPos = sprPlayer.Position() + playerVelocity * deltaTime;
+    const Rect playerRect = sprPlayer.Rectangle();
+    if (newPos.x < -0.5f * (windowWidth - playerRect.size.x)) {
+      newPos.x = -0.5f * (windowWidth - playerRect.size.x);
+    } else if (newPos.x > 0.5f * (windowWidth - playerRect.size.x)) {
+      newPos.x = 0.5f * (windowWidth - playerRect.size.x);
+    }
+    if (newPos.y < -0.5f * (windowHeight - playerRect.size.y)) {
+      newPos.y = -0.5f * (windowHeight - playerRect.size.y);
+    } else if (newPos.y > 0.5f * (windowHeight - playerRect.size.y)) {
+      newPos.y = 0.5f * (windowHeight - playerRect.size.y);
+    }
+    sprPlayer.Position(newPos);
+  }
+  sprPlayer.Update(deltaTime);
+}
+
+/**
+* ゲームの状態を描画する..
+*
+* @param window ゲームを管理するウィンドウ.
+*/
+void render(GLFWEW::WindowRef window)
+{
+  renderer.BeginUpdate();
+  renderer.AddVertices(sprBackground);
+  renderer.AddVertices(sprPlayer);
+  renderer.EndUpdate();
+  renderer.Draw({ windowWidth, windowHeight });
+  window.SwapBuffers();
+}
+
